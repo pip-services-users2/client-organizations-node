0f8a021bc44b7f714d8db697ee3fbcc3460a829b
Danil Prisiazhnyi <judas.priest999@gmail.com>
2022-12-21 02:32:10 +0400

    fixed tests

--- test/version1/OrganizationsClientFixtureV1.ts
+++ test/version1/OrganizationsClientFixtureV1.ts
@@ -1,53 +1,57 @@
 const assert = require('chai').assert;
 
-import { PagingParams } from 'pip-services3-commons-nodex';
+import { IdGenerator, PagingParams } from 'pip-services3-commons-nodex';
 
 import { OrganizationV1 } from '../../src/version1/OrganizationV1';
 import { IOrganizationsClientV1 } from '../../src/version1/IOrganizationsClientV1';
 
-let ORGANIZATION1: OrganizationV1 = {
-    id: '1',
-    code: '111',
-    name: 'Organization #1',
-    description: 'Test organization #1',
-    create_time: new Date(),
-    creator_id: '123',
-    active: true
-};
-let ORGANIZATION2: OrganizationV1 = {
-    id: '2',
-    code: null,
-    name: 'Organization #2',
-    description: 'Test organization #2',
-    create_time: new Date(),
-    creator_id: '123',
-    active: true
-};
-
 export class OrganizationsClientFixtureV1 {
     private _client: IOrganizationsClientV1;
+
+    ORGANIZATION1: OrganizationV1;
+    ORGANIZATION2: OrganizationV1;
     
     constructor(client: IOrganizationsClientV1) {
         this._client = client;
+
+        this.ORGANIZATION1 = {
+            id: IdGenerator.nextLong(),
+            code: null,
+            name: 'Organization #1',
+            description: 'Test organization #1',
+            create_time: new Date(),
+            creator_id: '123',
+            active: true
+        };
+
+        this.ORGANIZATION2 = {
+            id: IdGenerator.nextLong(),
+            code: null,
+            name: 'Organization #2',
+            description: 'Test organization #2',
+            create_time: new Date(),
+            creator_id: '123',
+            active: true
+        };
     }
         
     public async testCrudOperations() {
         let organization1, organization2: OrganizationV1;
 
         // Create one organization
-        organization1 = await this._client.createOrganization(null, ORGANIZATION1);
+        organization1 = await this._client.createOrganization(null, this.ORGANIZATION1);
 
         assert.isObject(organization1);
-        assert.equal(organization1.name, ORGANIZATION1.name);
-        assert.equal(organization1.description, ORGANIZATION1.description);
+        assert.equal(organization1.name, this.ORGANIZATION1.name);
+        assert.equal(organization1.description, this.ORGANIZATION1.description);
         assert.isNotNull(organization1.code);
 
         // Create another organization
-        organization2 = await this._client.createOrganization(null, ORGANIZATION2);
+        organization2 = await this._client.createOrganization(null, this.ORGANIZATION2);
 
         assert.isObject(organization2);
-        assert.equal(organization2.name, ORGANIZATION2.name);
-        assert.equal(organization2.description, ORGANIZATION2.description);
+        assert.equal(organization2.name, this.ORGANIZATION2.name);
+        assert.equal(organization2.description, this.ORGANIZATION2.description);
         assert.isNotNull(organization2.code);
 
         // Get all organizations
@@ -73,7 +77,7 @@ export class OrganizationsClientFixtureV1 {
 
         assert.isObject(organization1);
         assert.equal(organization1.description, 'Updated Content 1');
-        assert.equal(organization1.name, ORGANIZATION1.name);
+        assert.equal(organization1.name, this.ORGANIZATION1.name);
 
         // Delete organization
         organization = await this._client.deleteOrganizationById(null, organization1.id);
